f40f2337ac405dd30f7f5d8f97bf02392d15aa83
Bloxion <angee.3@icloud.com>
2025-11-22 22:12:07 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,24 @@
 // src/index.ts
 import { HttpClient } from "./lib/http";
 import { UsersClient } from "./clients/users";
+import { GroupsClient } from "./clients/groups";
 
 export class Novum {
+  private http: HttpClient;
+
   public users: UsersClient;
+  public groups: GroupsClient;
 
   constructor(options?: { cookie?: string }) {
-    const http = new HttpClient(options?.cookie);
+    this.http = new HttpClient(options?.cookie);
 
-    this.users = new UsersClient(http);
+    this.users = new UsersClient(this.http);
+    this.groups = new GroupsClient(this.http);
+  }
+
+  /** Change the .ROBLOSECURITY cookie at runtime */
+  setCookie(cookie: string) {
+    this.http.setCookie(cookie);
   }
 }
 
